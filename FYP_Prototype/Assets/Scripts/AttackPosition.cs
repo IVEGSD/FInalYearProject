fc24e363d3db7aefe0245b6d95a429ad634cdd73
lamkakuen <34675989+lamkakuen@users.noreply.github.com>
2018-01-22 21:37:13 +0800

    ButtonPosition
    Attack Button follow the click location

---
+++ FYP_Prototype/Assets/Scripts/AttackPosition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AttackPosition : MonoBehaviour {
+	public Button yourButton;
+	//public GameObject thisO;
+	// Use this for initialization
+	void Start () {
+		Button abtn = yourButton.GetComponent<Button>();
+		//GameObject buttonA = GameObject.Find("Attack");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 posT = this.transform.position;
+		Vector3 pos = yourButton.transform.position;
+
+		pos.x = BoardM.selectedChessman.transform.position.x + 1.2f;
+		pos.z = BoardM.selectedChessman.transform.position.z + 0.8f;
+		pos.y = BoardM.selectedChessman.transform.position.y +0.5f;
+		//pos.z = BoardM.selectedChessman.transform.position.z;
+		//pos.y = BoardM.selectionY;
+		yourButton.transform.position = pos;
+	}
+}
--- FYP_Prototype/Assets/Scripts/TurnEnd.cs
+++ FYP_Prototype/Assets/Scripts/TurnEnd.cs
@@ -14,6 +14,8 @@ public class TurnEnd : MonoBehaviour {
 		
 		Button btn = yourButton.GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
+
+	
 	}
 
 	void TaskOnClick()
