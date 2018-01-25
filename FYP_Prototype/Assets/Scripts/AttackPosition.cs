c9c2507c676a1ac8f4d2aa1fdf3f4759d3a5de5a
lamkakuen <34675989+lamkakuen@users.noreply.github.com>
2018-01-25 21:41:39 +0800

    ItemButtomAndSkillButton
    ItemButtomAndSkillButton follow the character location

--- FYP_Prototype/Assets/Scripts/AttackPosition.cs
+++ FYP_Prototype/Assets/Scripts/AttackPosition.cs
@@ -5,10 +5,14 @@ using System.Collections;
 
 public class AttackPosition : MonoBehaviour {
 	public Button yourButton;
+	public Button itemButton;
+	public Button skillButton;
 	//public GameObject thisO;
 	// Use this for initialization
 	void Start () {
 		Button abtn = yourButton.GetComponent<Button>();
+		Button ibtn = itemButton.GetComponent<Button>();
+		Button sbtn = skillButton.GetComponent<Button>();
 		//GameObject buttonA = GameObject.Find("Attack");
 	}
 	
@@ -23,5 +27,23 @@ public class AttackPosition : MonoBehaviour {
 		//pos.z = BoardM.selectedChessman.transform.position.z;
 		//pos.y = BoardM.selectionY;
 		yourButton.transform.position = pos;
+
+		Vector3 pos2 = itemButton.transform.position;
+
+		pos2.x = BoardM.selectedChessman.transform.position.x + 1.2f;
+		pos2.z = BoardM.selectedChessman.transform.position.z + -0.4f;
+		pos2.y = BoardM.selectedChessman.transform.position.y +0.5f;
+		//pos.z = BoardM.selectedChessman.transform.position.z;
+		//pos.y = BoardM.selectionY;
+		itemButton.transform.position = pos2;
+
+		Vector3 pos3 = skillButton.transform.position;
+
+		pos3.x = BoardM.selectedChessman.transform.position.x + 1.2f;
+		pos3.z = BoardM.selectedChessman.transform.position.z + 0.2f;
+		pos3.y = BoardM.selectedChessman.transform.position.y +0.5f;
+		//pos.z = BoardM.selectedChessman.transform.position.z;
+		//pos.y = BoardM.selectionY;
+		skillButton.transform.position = pos3;
 	}
 }
