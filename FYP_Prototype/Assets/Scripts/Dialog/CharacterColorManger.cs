653481db3b3472cad49e7782a6538071bbfd4711
lamkakuen <34675989+lamkakuen@users.noreply.github.com>
2018-01-29 20:54:48 +0800

    1
    1

---
+++ FYP_Prototype/Assets/Scripts/Dialog/CharacterColorManger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterColorManger : MonoBehaviour {
+
+    public Animator colorEffect;
+
+    void Start()
+    {
+        colorEffect.SetBool("isBlack", false);
+    }
+    
+    //public void turnToBlack()
+    //{
+    //    colorEffect.SetBool("isBlack", true);
+    //}
+
+    //public void turnToFull()
+    //{
+    //    colorEffect.SetBool("isBlack", false);
+    //}
+    
+    public void ChangeBlack(bool isBlack)
+    {
+        if (isBlack)
+        {
+            colorEffect.SetBool("isBlack", true);
+        }
+        else
+        {
+            colorEffect.SetBool("isBlack", false);
+        }
+    }
+}
---
+++ FYP_Prototype/Assets/Scripts/Dialog/FadeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeManager : MonoBehaviour {
+
+    public Animator fadeEffect;
+
+    [Header("Get the dialogue is end or not")]
+    public Animator dialogueAnimation;  //showing the dialogue -> button to top animation
+
+    void Start () {
+        fadeEffect.SetBool("isFadeIn", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(dialogueAnimation.GetBool("IsOpen") == false)
+        {
+            fadeEffect.SetBool("isFadeIn", false);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            fadeEffect.SetBool("isFadeIn", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            fadeEffect.SetBool("isFadeIn", true);
+        }
+    }
+}
